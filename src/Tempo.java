7bdf6204d627e99e5acccebcad0cd9536142f175
Carlos <kadukitesesi@gmail.com>
2024-06-12 17:28:29 -0300

    Apenas exercitando

---
+++ src/Tempo.java
@@ -0,0 +1,14 @@
+
+import java.time.LocalTime;
+
+public class Tempo {
+    public static void main(String[] args) {
+        String horaString = "12:30";
+        LocalTime horaLocal = LocalTime.parse(horaString);
+        int horaLocalMinutos = horaLocal.toSecondOfDay() / 60;
+
+
+
+        System.out.println(horaLocalMinutos);
+    }
+}
